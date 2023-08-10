971b1aa290c105c61bc38c020cab9199f4f6396d
dhuy198 <119914938+dhuy198@users.noreply.github.com>
2023-08-10 12:24:31 +0700

    khop_cau.cpp

--- khop_cau.cpp
+++ khop_cau.cpp
@@ -42,28 +42,3 @@ void dfs(int u, int parent)
         }
     }
 }
-
-int32_t main()
-{
-    // freopen("input.in", "r", stdin);
-    // freopen("output.out", "w", stdout);
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n >> m;
-    int u, v;
-    for (int i = 1; i <= m; i++)
-    {
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (!num[i])
-            dfs(i, i);
-    }
-    for (int i = 1; i <= n; i++)
-        if (isJoint[i])
-            cout << i << ' ';
-}
